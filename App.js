417e3c484d2133ed401f14983624aafe45a53bf8
MeroshiniRaja <meroshiniraja19@gmail.com>
2023-04-18 11:54:46 +0530

    Episode-02

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 const parent = React.createElement("div",{id:"heading"},"Hello from React!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
